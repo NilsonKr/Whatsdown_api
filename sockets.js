13b8d31f8dd0eb373d18ccd40664edf986e1f876
nilsonkr <nuotrok444@gmail.com>
2021-07-05 00:36:05 -0500

    [ADD] Sockets chat room connections and messaging

--- sockets.js
+++ sockets.js
@@ -5,14 +5,30 @@ const socket = {};
 function connect(server) {
 	socket.io = SocketIO(server, {
 		cors: {
-			origin: 'http://localhost:8000',
+			origin: '*',
 			methods: ['GET', 'POST'],
 		},
 	});
 
-	socket.io.on('connection', socket => {
+	socket.io.on('connection', extSocket => {
 		console.log('[SOCKET] New Connection!');
-		socket.emit('message', 'Welcome!');
+		extSocket.emit('message', 'Welcome!');
+
+		//Join a chat room
+		extSocket.on('join chat', chat => {
+			extSocket.join(chat);
+		});
+
+		//Send a msg through chat room
+		extSocket.on('private', msg => {
+			extSocket.to(msg.chatId).emit('chatmsg', msg.message);
+		});
+
+		//Finish connection
+		extSocket.on('close', () => {
+			extSocket.end();
+			console.log('close');
+		});
 	});
 }
 
