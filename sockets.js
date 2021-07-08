43158b94d3f80db56ac940cba989a3354348d862
NilsonKr <nuotrok444@gmail.com>
2021-07-08 17:29:50 -0500

    :sparkles: [ADD] Socket dedicated for user and New chat event to update client

--- sockets.js
+++ sockets.js
@@ -14,6 +14,17 @@ function connect(server) {
 	socket.io.on('connection', extSocket => {
 		console.log('[SOCKET] New Connection!');
 
+		//Join a User dedicated room
+		extSocket.on('user room', userId => {
+			extSocket.join(userId);
+			// console.log(extSocket.rooms);
+		});
+
+		//Create a new Chat room
+		extSocket.on('new chat', chatInfo => {
+			socket.io.to(chatInfo.toUserId).emit('new chat client', chatInfo.chat);
+		});
+
 		//Join a chat room
 		extSocket.on('join chat', chat => {
 			extSocket.join(chat);
