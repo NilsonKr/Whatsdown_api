bcfa2895fbbe7ec2588c599d79f2807135d97931
NilsonKr <nuotrok444@gmail.com>
2021-07-10 16:11:05 -0500

    :sparkles: [ADD] Webpack config for prod

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const NodeExternals = require('webpack-node-externals');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const DotenvPlugin = require('dotenv-webpack');
+
+/**
+ * @type {import('webpack').Configuration}
+ */
+
+module.exports = {
+	entry: './index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	resolve: {
+		extensions: ['.js'],
+	},
+	mode: 'production',
+	target: 'node',
+	externalsPresets: { node: true },
+	externals: [NodeExternals()],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: 'babel-loader',
+			},
+		],
+	},
+	plugins: [new CleanWebpackPlugin(), new DotenvPlugin()],
+};
